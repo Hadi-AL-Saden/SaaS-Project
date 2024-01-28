eb7a58ddccb08bef4682802feb149bc1b3b28bd3
unknown <hadialsaden@gmail.com>
2024-01-28 17:36:15 +0300

    second push

--- saas-ai/app/(landing)/page.tsx
+++ saas-ai/app/(landing)/page.tsx
@@ -1,14 +1,23 @@
+import {Button} from "@/components/ui/button"
 import { UserButton } from "@clerk/nextjs";
+import Link from "next/link";
 function Landing() {
 return (
-    <>
- <UserButton afterSignOutUrl="/"/>
- <div>Landing</div>
+<>
+    <UserButton afterSignOutUrl="/" />
 
+    <div>Landing</div>
+    <div className="flex display-flex justify-center  gap-6">
+        <Link href={"/sign-in"}>
+            <Button>Login</Button>
+        </Link>
 
-    </>
+        <Link href={"/sign-up"}>
+            <Button>Sign-up</Button>
+        </Link>
+    </div>
 
-
-)
+</>
+);
 }
 export default Landing;
